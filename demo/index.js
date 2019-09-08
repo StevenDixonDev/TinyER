157eab1316d6ada25e77e4946fccbd1f78db5bdd
Steven Dixon <stevendixon112@gmail.com>
2019-09-07 23:38:21 -0400

    added demo and fixed things for heroku

--- demo/index.js
+++ demo/index.js
@@ -4,11 +4,16 @@ const exphbs  = require('express-handlebars');
 const path = require("path");
 const static = require('serve-static');
 
+const PORT = process.env.PORT || 3000;
 // create router
 const router = tinyER.Router();
 // create application
 const app = tinyER();
 
+// set a route on the router
+router.get('/', (req, res)=>{
+  res.send('This is a sub-route');
+})
 
 // set views location
 app.set('views', path.join(__dirname, 'views'));
@@ -17,15 +22,15 @@ app.set('view engine', 'handlebars');
 // set handlebars to the actual engine function
 app.engine('handlebars', exphbs());
 
-
 //set app to use middleware
 app.use(testMiddleWare);
 app.use(bodyParser.json());
 app.use(static(__dirname + '/public'));
+// use a router object as a route
+app.use('/test', router);
 
 // example of a post request
 app.post('/', (req, res)=>{
-  console.log(req.body);
   res.status(200).json("thank you");
 });
 
@@ -45,7 +50,6 @@ app.get('/Middleware', (req, res) =>{
 });
 
 app.post('/Middleware', (req, res) =>{
-  console.log(req.body)
   res.json(req.body);
 });
 
@@ -58,30 +62,29 @@ app.get('/test', (req, res) =>{
   res.render('index', { title: 'my other page', layout: 'second' });
 });
 
+// sendfile example
+app.get('/sendfile', (req, res)=>{
+  res.sendFile(__dirname + "/index.html");
+});
 
-
-// // sendfile example
-// app.get('/test', (req, res)=>{
-//   res.sendFile(__dirname + "/index.html");
-// });
-
-// // redirect example
-// app.get('/t', (req, res)=>{
-//   res.redirect('/test');
-// })
+// redirect example
+app.get('/redirect', (req, res)=>{
+  res.redirect('/sendfile');
+})
 
 // handle bars example
 app.get('/handlebar', (req, res)=>{  
   res.render("test", {lay: false , t: "<p>Hi I am a handlebars demo</p>"});
 });
 
+// redirect all non matched routes
 app.get('*', (req, res) => {
   res.redirect('/');
 })
 
 // tell the app to  listen on port 8080
-app.listen(8080, ()=>{
-  console.log('running on 8080')
+app.listen(PORT, ()=>{
+  console.log('running on port: ' + PORT)
 })
 
 // define the middleware
